4f7043ed07dc6163bc75caa59bc5020b01c34b20
francoleon42 <francoleonadress@gmail.com>
2025-02-05 13:51:10 -0300

    add configuracion db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { DocumentModule } from './document/document.module';
-import { RentModule } from './rent/rent.module';
-import { CarModule } from './car/car.module';
-import { PictureModule } from './picture/picture.module';
+import { User } from './user/entities/user.entity';
+import { Document } from './document/entities/document.entity';
+import { Rent } from './rent/entities/rent.entity';
+import { Car } from './car/entities/car.entity';
+import { Picture } from './picture/entities/picture.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
   imports: [
-    UserModule,
-    DocumentModule,
-    RentModule,
-    CarModule,
-    PictureModule,
+    User,
+    Document,
+    Rent,
+    Car,
+    Picture,
     TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
-      entities: [CarModule, PictureModule,RentModule,DocumentModule,UserModule],
-      synchronize: true,
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: 3306,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [Car, Picture,Rent,Document,User], 
+      synchronize: true, 
+      logging: true,  
     }),
   ],
   controllers: [AppController],

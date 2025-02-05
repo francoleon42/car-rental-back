4b60f4f57b190689cac556859a6c0e0c6825b76a
francoleon42 <francoleonadress@gmail.com>
2025-02-05 11:16:35 -0300

    creacion de entidad documento y user

--- src/document/entities/document.entity.ts
+++ src/document/entities/document.entity.ts
@@ -1,1 +1,30 @@
-export class Document {}
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import { User } from '../../user/entities/user.entity';
+
+@Entity()
+export class Document {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  url: string;
+
+  @Column()
+  src: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  title: string;
+
+  @Column({ type: 'timestamp', nullable: true }) 
+  createdAt: Date;
+  
+  @Column({ type: 'timestamp', nullable: true }) 
+  updatedAt: Date;
+
+  @ManyToOne(() => User, (user) => user.documents, { onDelete: 'CASCADE' })
+  user: User;
+  
+}
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,45 @@
-export class User {}
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+import { Document } from '../../document/entities/document.entity';
+import { Role } from '../../common/enums/role.enum';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column({ type: 'date' })
+  dob: Date;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  address: string;
+
+  @Column()
+  country: string;
+
+  @Column({
+    type: 'enum',
+    enum: Role,
+    nullable: false,
+  })
+  role: Role;
+
+  @OneToMany(() => Document, (document) => document.user)
+  documents: Document[];
+
+  @Column({ type: 'timestamp', nullable: true }) 
+  createdAt: Date;
+  
+  @Column({ type: 'timestamp', nullable: true }) 
+  updatedAt: Date;
+  
+
+}

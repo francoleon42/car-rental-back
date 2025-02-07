19d220c290a0fc4682edcce6403788cef4e3c78b
francoleon42 <francoleonadress@gmail.com>
2025-02-06 21:34:03 -0300

    refactor test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,7 +3,7 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
-jest.setTimeout(30000);
+jest.setTimeout(60000);
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -34,68 +34,45 @@ describe('AppController (e2e)', () => {
     expect(response.body).toEqual(
       expect.arrayContaining([
         expect.objectContaining({
-          id: 1,
-          brand: 'Toyota',
-          model: 'Corolla',
-          color: 'Red',
-          passengers: 5,
-          ac: true,
-          pricePerDay: 50,
-        }),
-        expect.objectContaining({
-          id: 2,
-          brand: 'Honda',
-          model: 'Civic',
-          color: 'Blue',
-          passengers: 5,
-          ac: true,
-          pricePerDay: 45,
-        }),
-      ]),
-    );
-  });
-
-
-  it('/cars/detalle/:id (GET) obtener los detalles cars', async () => {
-    const carId = 1;
-    const response = await request(app.getHttpServer())
-      .get(`/cars/detalle/${carId}`);
-
-    expect(response.body).toEqual(
-      expect.objectContaining({
-        carResponseDTO: expect.objectContaining({
-          id: carId,
+          id: expect.any(Number),
           brand: expect.any(String),
           model: expect.any(String),
           color: expect.any(String),
           passengers: expect.any(Number),
           ac: expect.any(Boolean),
           pricePerDay: expect.any(Number),
-          createdAt: expect.any(String),
+        }),
+      ]),
+    );
+  });
+
+
+
+  it('/cars/detalle/:id (GET) obtener los detalles cars', async () => {
+    const carId = 1;
+    const response = await request(app.getHttpServer()).get(`/cars/detalle/${carId}`).expect(200);
+
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        carResponseDTO: expect.objectContaining({
+          id: expect.any(Number),
+          brand: expect.any(String),
+          model: expect.any(String),
+          color: expect.any(String),
+          passengers: expect.any(Number),
+          ac: expect.any(Boolean),
+          pricePerDay: expect.any(Number),
           img: expect.arrayContaining([
             expect.objectContaining({
               id: expect.any(Number),
               src: expect.any(String),
-              description: expect.any(String),
-              carPicture: expect.any(String),
-              createdAt: expect.any(String),
-              updatedAt: expect.any(String),
             }),
           ]),
         }),
-        picturesResponseDTO: expect.arrayContaining([
-          expect.objectContaining({
-            id: expect.any(Number),
-            src: expect.any(String),
-            description: expect.any(String),
-            carPicture: expect.any(String),
-            createdAt: expect.any(String),
-            updatedAt: expect.any(String),
-          }),
-        ]),
       }),
     );
   });
+
 
 });
 

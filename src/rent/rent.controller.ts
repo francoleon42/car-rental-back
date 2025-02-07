f4093ffa5dd6327f385d97132ec8d2bbed864137
francoleon42 <francoleonadress@gmail.com>
2025-02-07 15:26:01 -0300

    add ver historial de solicitudes de usuario

--- src/rent/rent.controller.ts
+++ src/rent/rent.controller.ts
@@ -6,6 +6,7 @@ import { User } from '../user/entities/user.entity';
 import { UserRepository } from '../user/user.repository';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Role } from '../common/enums/role.enum';
 
 @Controller('rent')
 export class RentController {
@@ -21,33 +22,60 @@ export class RentController {
   async create(@Body() createRentDto: CreateRentDto) {
     //TODO : modificar con obtener el usuario logueado
     const id = 1;
-    const user = await await this.userRepository.findOneBy({ id });
+    const user =  await this.userRepository.findOneBy({ id: id });
     if (!user) {
       throw new Error('Usuario no encontrado');
     }
     return this.rentService.create(createRentDto, user);
   }
 
+// Quiero ver el historial de mis solicitudes de alquiler (ver todas mis solicitudes rentas)
+  @Get('/mis_solicitudes')
+  async  obtenerSolicitudesDeUsuario() {
+    //TODO : modificar con obtener el usuario logueado
+    const id = 1;
+    const user =  await this.userRepository.findOneBy({ id: id });
+    if (!user) {
+      throw new Error('Usuario no encontrado');
+    }
+    return this.rentService.obtenerSolicitudesDeUsuario(user);
+  }
   // admin :
   //Como admin voy a querer listar todas las solicitudes de rent
   // ( todas las rentas sin aceptar)
   @Get('/solicitadas')
-  findAll() {
+  obtenerRentSolicitadas() {
     return this.rentService.obtenerRentSolicitadas();
   }
 
-//TODO:  el admin va a poder aceptar o rechazar una renta
+// El admin va a poder aceptar o rechazar una renta
   @Patch('/aceptar/:id')
-  rechazar(@Param('id') id: number, @Body() updateRentDto: UpdateRentDto) {
-    return this.rentService.update(+id, updateRentDto);
-  }
-  @Patch('/rechazar:id')
-  aceptar(@Param('id') id: number, @Body() updateRentDto: UpdateRentDto) {
-    return this.rentService.update(+id, updateRentDto);
+  async rechazar(@Param('id') id: number) {
+    //TODO : modificar con obtener el admin logueado que va aceptar la rent y validar que sea admin
+    const idAdmin = 2;
+    const userAdmin =  await this.userRepository.findOneBy({ id: idAdmin });
+    if (!userAdmin) {
+      throw new Error('Usuario Admin no encontrado');
+    }
+    if (userAdmin.role !== Role.ADMIN) {
+      throw new Error('El usuario no tiene rol de administrador');
+    }
+    return this.rentService.aceptar(userAdmin,+id);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.rentService.remove(+id);
+  @Patch('/rechazar/:id')
+  async aceptar(@Param('id') id: number) {
+
+    //TODO : modificar con obtener el admin logueado que va aceptar la rent y validar que sea admin
+    const idAdmin = 2;
+    const userAdmin =  await this.userRepository.findOneBy({ id: idAdmin });
+    if (!userAdmin) {
+      throw new Error('Usuario Admin no encontrado');
+    }
+    if (userAdmin.role !== Role.ADMIN) {
+      throw new Error('El usuario no tiene rol de administrador');
+    }
+    return this.rentService.rechazar(userAdmin,id);
   }
+
 }
--- src/rent/rent.service.ts
+++ src/rent/rent.service.ts
@@ -1,14 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateRentDto } from './dto/create-rent.dto';
 import { UpdateRentDto } from './dto/update-rent.dto';
 import { User } from '../user/entities/user.entity';
 import { Rent } from './entities/rent.entity';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Column, CreateDateColumn, IsNull, ManyToOne, MoreThan, Repository, UpdateDateColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  IsNull,
+  ManyToOne,
+  MoreThan,
+  PrimaryGeneratedColumn,
+  Repository,
+  UpdateDateColumn,
+} from 'typeorm';
 import { CarService } from '../car/car.service';
 import { plainToInstance } from 'class-transformer';
 import { CarResponseDTO } from '../car/dto/car-response-dto';
 import { ResponseRentDto } from './dto/response-rent.dto';
+import { Car } from '../car/entities/car.entity';
 
 @Injectable()
 export class RentService {
@@ -42,6 +52,7 @@ export class RentService {
     };
   }
 
+
   async obtenerRentSolicitadas() {
     const rents: Rent[] = await this.rentRepository.find({
       where: {
@@ -57,15 +68,78 @@ export class RentService {
     );
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} rent`;
+  async obtenerSolicitudesDeUsuario(usuario: User) {
+    const rents = await this.rentRepository.find({
+      where: { user: usuario },
+      relations: ['car', 'admin'],
+    });
+    return rents.map(rent =>
+      plainToInstance(ResponseRentDto, rent, {
+        excludeExtraneousValues: true,
+        enableImplicitConversion: true,
+      }),
+    );
+  }
+
+
+  async aceptar(admin: User, id: number) {
+    const rent = await this.rentRepository.findOne({
+      where: { id },
+      relations: ['admin']
+    });
+
+    if (!rent) {
+      throw new NotFoundException(`Rent con ID ${id} no encontrado`);
+    }
+    if (rent.admin) {
+      throw new BadRequestException(`Rent ${id} ya está aprobado o rechazado`);
+    }
+    rent.rejected = false;
+    rent.admin = admin;
+    rent.acceptedDate = new Date();
+    rent.updatedAt = new Date();
+
+    await this.rentRepository.save(rent);
+    return {
+      mensaje: "Rent aprobada",
+      idRenta: rent.id,
+      rejected: rent.rejected,
+      acceptedDate :  rent.acceptedDate,
+      admin:   rent.admin.email,
+      updatedAt: rent.updatedAt,
+    };
+  }
+
+  async rechazar(admin: User, id: number) {
+    const rent = await this.rentRepository.findOne({
+      where: { id },
+      relations: ['admin']
+    });
+    if (!rent) {
+      throw new NotFoundException(`Rent con ID ${id} no encontrado`);
+    }
+    if (rent.admin) {
+      throw new BadRequestException(`Rent ${id} ya está aprobado o rechazado`);
+    }
+    rent.rejected = true;
+    rent.admin = admin;
+    rent.acceptedDate = null;
+    rent.updatedAt = new Date();
+
+    await this.rentRepository.save(rent);
+
+    return {
+      mensaje: "Rent rechazada",
+      idRenta: rent.id,
+      rejected: rent.rejected,
+      acceptedDate :  rent.acceptedDate,
+      admin:   rent.admin.email,
+      updatedAt: rent.updatedAt,
+    };
   }
 
   update(id: number, updateRentDto: UpdateRentDto) {
     return `This action updates a #${id} rent`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} rent`;
-  }
 }
